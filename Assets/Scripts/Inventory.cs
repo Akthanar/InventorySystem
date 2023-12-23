091ea9a68c485662944a81739e5e6f9929e9d333
Akthanar <highshore.studio@gmail.com>
2023-12-24 00:56:46 +0100

    upload project

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] private bool isInventoryOpen = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
